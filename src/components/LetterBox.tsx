4a05827f81c5a2ce17174391d2d0ac9775992343
Xiaolin Zhan <zhanxl@live.com>
2023-03-01 20:33:06 -0800

    change to hello world

---
+++ src/components/LetterBox.tsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class LetterBox extends Component {
+  render() {
+    return (
+        <div>
+
+        </div>
+    );
+  }
+}
+
+LetterBox.propTypes = {};
+
+export default LetterBox;
