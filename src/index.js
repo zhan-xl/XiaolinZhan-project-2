d8261e4624e355cf1cc338265419dccec1d44824
Xiaolin Zhan <zhanxl@live.com>
2023-03-15 15:44:50 -0700

    commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
+import LetterBox from "./components/wordle";
+import './App.css'
+import Wordle from "./components/wordle";
 
 const element = <h1>Hello World</h1>;
-createRoot(document.getElementById('root')).render(element);
+createRoot(document.getElementById('root')).render(<Wordle />);
